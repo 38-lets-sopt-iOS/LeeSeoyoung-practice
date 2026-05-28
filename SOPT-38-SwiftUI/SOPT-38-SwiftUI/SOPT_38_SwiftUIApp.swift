1b28840cdfee3fd8e4ecdf3fbd68d8ee2e2db7fb
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-28 10:46:39 +0900

    chore: SwiftUI 프로젝트 추가

---
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/SOPT_38_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT_38_SwiftUIApp.swift
+//  SOPT-38-SwiftUI
+//
+//  Created by Seoyoung Lee on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SOPT_38_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GridExampleView()
+        }
+    }
+}
