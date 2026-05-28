12ad8e065850d1026a07c39a2ff845082a75e6bf
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-29 01:57:40 +0900

    fix: 여백 조정

--- SOPT-38-SwiftUI/SOPT-38-SwiftUI/week6/ContentView.swift
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/week6/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             StoryScrollVIew(chatUsers: ChatUser.users, activeStates: $activeStates)
             Spacer().frame(height: 20)
             ChatListView(chatUsers: ChatUser.users, activeStates: $activeStates)
+            Spacer()
         }
     }
 }
